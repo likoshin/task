715bb1436541793af065b1bf81e12cb1913fea43
likoshin <74928664+likoshin@users.noreply.github.com>
2023-09-20 08:26:35 +0300

    Add files via upload

---
+++ task_2/task_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+namespace task_2
+{
+    class Cat
+    {
+        static void Main(string[] args)
+        {
+
+
+            while(true)
+                {
+                    Console.WriteLine("──────▄▀▄─────▄▀▄");
+                    Console.WriteLine("─────▄█░░▀▀▀▀▀░░█▄");
+                    Console.WriteLine("─▄▄──█░░░░░░░░░░░█──▄▄");
+                    Console.WriteLine("█▄▄█─█░░▀░░┬░░▀░░█─█▄▄█");
+                    
+                    Thread.Sleep(500);
+                    Console.Clear();
+                    Console.WriteLine("──────▄▀▄─────▄▀▄");
+                    Console.WriteLine("─────▄█░░▀▀▀▀▀░░█▄");
+                    Console.WriteLine("─▄▄──█░░░░░░░░░░░█──▄▄");
+                    Console.WriteLine("█▄▄█─█░░_░░┬░░_░░█─█▄▄█");
+                    
+                    Thread.Sleep(500);
+                    Console.Clear();
+
+
+
+                }
+               
+
+        }
+
+    }
+}
